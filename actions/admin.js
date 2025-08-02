da7de7240341895f1af0e71f4bfabf2dc176e460
siddharth007-singh <siddharth.code.cws@gmail.com>
2025-08-03 03:35:44 +0530

    Admin Backend

---
+++ actions/admin.js
@@ -0,0 +1,172 @@
+"use server";
+
+
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+import { db } from "@/lib/prisma";
+
+export const verifyAdmin = async () => {
+    const { userId } = await auth();
+    if (!userId) {
+        return false;
+    }
+
+    try {
+        const user = await db.user.findUnique({
+            where: {
+                clerkUserId: userId,
+            },
+        });
+
+        return user?.role === "ADMIN";
+    }
+    catch (error) {
+        console.error("Failed to verify admin:", error);
+        return false;
+    }
+}
+
+export const getAllPendingDoctors = async () => {
+    const isAdmin = await verifyAdmin();
+    if (!isAdmin) throw new Error("Unauthorized");
+
+
+    try {
+        const pendingDoctors = await db.doctor.findMany({
+            where: {
+                role: "DOCTOR",
+                verificationStatus: "PENDING",
+            },
+            orderBy: {
+                createdAt: "desc",
+            },
+        });
+
+        return { doctors: pendingDoctors };
+    }
+    catch (error) {
+        throw new Error("Failed to fetch pending doctors");
+    }
+}
+
+
+export const getVerifiedDoctors = async () => {
+    const isAdmin = await verifyAdmin();
+    if (!isAdmin) throw new Error("Unauthorized");
+
+    try{
+        const verifiedDoctors = await db.doctor.findMany({
+            where: {
+                role: "DOCTOR",
+                verificationStatus: "VERIFIED",
+            },
+            orderBy: {
+                name: "asc",
+            },
+        });
+
+        return { doctors: verifiedDoctors };
+    }
+    catch(error){
+        console.error("Failed to fetch verified doctors:", error);
+        throw new Error("Failed to fetch verified doctors");
+    }
+}
+
+
+export const updateDoctorStatus = async (formData) => {
+    const isAdmin = await verifyAdmin();
+    if (!isAdmin) throw new Error("Unauthorized");
+
+    try{
+        const doctorId = formData.get("doctorId");
+        const status = formData.get("status");
+
+        if (!doctorId || !status) {
+            throw new Error("Doctor ID and status are required");
+        }
+
+        await db.user.update({
+            where: { id: doctorId },
+            data: { verificationStatus: status },
+        });
+
+        revalidatePath("/admin");
+
+        return {success: true, message: "Doctor verification status updated successfully"};
+    }
+    catch(error) {
+        console.error("Failed to update doctor verification status:", error);
+        throw new Error("Failed to update doctor verification status");
+    }
+}
+
+
+export const updateDoctorActiveStatus = async (formData) => {
+    try{
+        const isAdmin = await verifyAdmin();
+        if (!isAdmin) throw new Error("Unauthorized");
+
+        const doctorId = formData.get("doctorId");
+        const suspend = formData.get("suspend") === "true";
+
+
+        if (!doctorId) {
+            throw new Error("Doctor ID is required");
+        }
+
+        const status = suspend ? "PENDING" : "VERIFIED";
+
+        await db.user.update({
+            where: { id: doctorId },
+            data: { verificationStatus: status },
+        });
+        revalidatePath("/admin");
+        return { success: true };
+    }  
+    catch(error) {
+        console.error("Failed to update doctor active status:", error);
+        throw new Error("Failed to update doctor active status");
+    }
+}
+
+
+//Merge both the Fucntionality of updateDoctorStatus and updateDoctorActiveStatus
+// export const updateDoctorStatu = async (formData) => {
+//   const isAdmin = await verifyAdmin();
+//   if (!isAdmin) throw new Error("Access denied: Admins only");
+
+//   const doctorId = formData.get("doctorId");
+//   const status = formData.get("status"); // Manual override
+//   const suspend = formData.get("suspend"); // "true" or "false"
+
+//   if (!doctorId) throw new Error("Doctor ID is required");
+
+//   // Determine new status
+//   const newStatus = status 
+//     ? status 
+//     : suspend === "true" 
+//       ? "PENDING" 
+//       : "VERIFIED";
+
+//   try {
+//     await db.user.update({
+//       where: { id: doctorId },
+//       data: { verificationStatus: newStatus },
+//     });
+
+//     revalidatePath("/admin");
+
+//     return {
+//       success: true,
+//       message: `Doctor status updated to ${newStatus}`,
+//     };
+//   } catch (error) {
+//     console.error("❌ Update failed:", error);
+//     throw new Error("Failed to update doctor status. Try again.");
+//   }
+// };
+
+
+
